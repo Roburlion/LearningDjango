509b377a70ee24b72e9ceab75ae90842a1633ee8
Roburlion <94622793+Roburlion@users.noreply.github.com>
2022-01-18 19:02:10 -0600

    3 - 3 complete ps - the previous commit should have said 3-2 complete

--- BasicProject/HelloDjangoApp/views.py
+++ BasicProject/HelloDjangoApp/views.py
@@ -10,10 +10,19 @@ def index(request):
     return render(
         request,
         "HelloDjangoApp/index.html",  # Relative path from the 'templates' folder to the template file
-        # "index.html", # Use this code for VS 2017 15.7 and earlier
         {
             'title' : "Hello Django",
             'message' : "Hello Django!",
             'content' : " on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+    )
+
+def about(request):
+    return render(
+        request,
+        "HelloDjangoApp/about.html",
+        {
+            'title' : "About HelloDjangoApp",
+            'content' : "Example app page for Django."
         }
     )
